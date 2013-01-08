472842e6f1d41008bc10d79cdaeda7acc4500251
manabe shinya <nantokosinuke@yahoo.co.jp>
2013-01-08 17:15:31 +0900

    set key

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 Myapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  ENV['MYAPP_CONSUMER_ID']="2adfbdc49b4c906f1533";
-  ENV['MYAPP_CONSUMER_SECRET']="886320d3845a01e4f02e02a43fea39700913c969";
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = 'bdafebe43e75aef14879b3add59ec536d9328a4baf6a11f784043ed9235de804e1d859eda976d64eea8a19dc0f3e6ae3e786713eb45a2607c08a441fca743617'
+Myapp::Application.config.secret_token = ENV['MYAPP_SECRET_TOKEN']
